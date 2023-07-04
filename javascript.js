67bcffb9c46bcbb1fc562ae006dc56bc3127e8c2
Henry Liu <hliu4176@gmail.com>
2023-07-04 12:56:20 -0500

    Add function header comments

--- javascript.js
+++ javascript.js
@@ -1,21 +1,35 @@
 setup();
 
+/**
+ * Set up environment including the option panel, grid, and event listeners.
+ */
 function setup() {
     setupGrid();
     setupOptionEventListeners();
 }
 
+/**
+ * Set up grid
+ * @param {Number} size - the dimensions of the grid
+ */
 function setupGrid(size = 16) {
     displaySize(size);
     drawGrid(size);
     setupGridEventListeners();
 }
 
+/**
+ * Reset the grid by removing all of its children
+ */
 function resetGrid() {
     const grid = document.querySelector('#grid');
     grid.innerHTML = '';
 }
 
+/**
+ * Resize the grid when size-slider changes and change the display of size-slider text
+ * @param {*} e 
+ */
 function resizeGrid(e) {
     const size = e.currentTarget.value;
     
@@ -24,6 +38,10 @@ function resizeGrid(e) {
     displaySize(size);
 }
 
+/**
+ * Draw the grid with provided size
+ * @param {Number} size - the dimensions of the grid
+ */
 function drawGrid(size) {
     const grid = document.querySelector('#grid');
 
@@ -38,6 +56,9 @@ function drawGrid(size) {
     }
 }
 
+/**
+ * Set up event listeners for the grid
+ */
 function setupGridEventListeners() {
     const squares = document.querySelectorAll('.square');
     squares.forEach(square => {
@@ -45,16 +66,27 @@ function setupGridEventListeners() {
     })
 }
 
+/**
+ * Set up event listeners for the option panel
+ */
 function setupOptionEventListeners() {
     const sizeSlider = document.querySelector('#size-slider');
     sizeSlider.addEventListener('change', resizeGrid);
 }
 
+/**
+ * Display the value of the size-slider
+ * @param {Number} size 
+ */
 function displaySize(size) {
     const sizeText = document.querySelector(['label[for="size-slider"']);
     sizeText.textContent = `Size: ${size}`;
 }
 
+/**
+ * A helper method to create individual squares of the grid
+ * @returns an element representing a square
+ */
 function createSquare() {
     const square = document.createElement('div');
     square.classList.add('square');
@@ -62,6 +94,10 @@ function createSquare() {
     return square;
 }
 
+/**
+ * Change the color of the square that the moused hovered over
+ * @param {Event} e - the event that triggers this function
+ */
 function color(e) {
     const square = e.currentTarget;
     square.classList.add('colored');
